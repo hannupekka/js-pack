23dca0415aed69cae8d3a18adbbf291682a52705
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2018-02-17 11:17:05 +0200

    Replace webpack with parcel.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import 'dom4';
 import 'normalize.css/normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'main.less';
+import '../less/main.less';
 
 // Require our JS.
-import 'scripts.js';
+import './scripts.js';
